2c0a31552ceedb54d5bc01481b3e9cbc8c2edd5e
Jakub Stasiorowski <kuba.stasiorowski@wp.pl>
2023-11-04 20:55:15 +0100

    Remove old draw functions

--- FlatEngine/Draw.h
+++ FlatEngine/Draw.h
@@ -21,24 +21,3 @@ public:
 		, b(b)
 	{}
 };
-
-namespace draw
-{
-	// TODO: Let's remove this
-	
-	// narysowanie na ekranie screen powierzchni sprite w punkcie (x, y)
-	// (x, y) to punkt œrodka obrazka sprite na ekranie
-	void DrawSurface(SDL_Surface* screen, SDL_Surface* sprite, int x, int y);
-
-	// rysowanie pojedynczego pixela
-	// draw a single pixel
-	void DrawPixel(SDL_Surface* surface, int x, int y, Uint32 color);
-
-	// rysowanie linii o d³ugoœci l w pionie (gdy dx = 0, dy = 1) 
-	// b¹dŸ poziomie (gdy dx = 1, dy = 0)
-	void DrawLine(SDL_Surface* screen, int x, int y, int l, int dx, int dy, Uint32 color);
-
-	// rysowanie prostok¹ta o d³ugoœci boków l i k
-	void DrawRectangle(SDL_Surface* screen, int x, int y, int l, int k,
-		Uint32 outlineColor, Uint32 fillColor);
-}
