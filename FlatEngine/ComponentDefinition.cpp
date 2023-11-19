4da884e9fe9010441e7698c30b7d393fffaac534
Jakub Stasiorowski <kuba.stasiorowski@wp.pl>
2023-11-19 19:02:49 +0100

    Move component field definition to separate file

--- FlatEngine/ComponentDefinition.cpp
+++ FlatEngine/ComponentDefinition.cpp
@@ -55,38 +55,3 @@ ComponentDefinition* ComponentDefinitionManager::GetDefinitionFromName(const std
 }
 
 
-ComponentFieldDefinition::ComponentFieldDefinition(const std::string& component_name, const std::string& field_name)
-	: m_ComponentName(component_name)
-	, m_FieldName(field_name)
-{
-	ComponentDefinitionManager::GetInstance().AddComponentField(component_name, *this);
-}
-
-const std::string& ComponentFieldDefinition::GetComponentName() const
-{
-	return m_ComponentName;
-}
-
-const std::string& ComponentFieldDefinition::GetFieldName() const
-{
-	return m_FieldName;
-}
-
-void ComponentFieldDefinition::SetFieldValue(ObjectComponent* component, const std::string& value) const
-{
-	void* parsed_value;
-	AllocateValue(parsed_value);
-	ParseValue(value, parsed_value);
-	SetFieldValue(component, parsed_value);
-}
-
-std::string ComponentFieldDefinition::GetFieldValue(ObjectComponent* component) const
-{
-	void* value_unstringified;
-	AllocateValue(value_unstringified);
-	GetFieldValue(component, value_unstringified);
-
-	std::string stringified_value;
-	ValueToString(value_unstringified, stringified_value);
-	return stringified_value;
-}
--- FlatEngine/ComponentDefinition.h
+++ FlatEngine/ComponentDefinition.h
@@ -1,55 +1,9 @@
 #pragma once
-#include <iostream>
-#include <functional>
 #include <map>
 #include <vector>
 
 #include "ObjectComponent.h"
-#include "SerializableTypes.h"
-
-class ComponentFieldDefinition
-{
-public:
-	ComponentFieldDefinition(const std::string& component_name, const std::string& field_name);
-
-	const std::string& GetComponentName() const;
-	const std::string& GetFieldName() const;
-	void SetFieldValue(ObjectComponent* component, const std::string& value) const;
-	std::string GetFieldValue(ObjectComponent* component) const;
-
-private:
-	virtual void SetFieldValue(ObjectComponent* component, void* value) const = 0;
-	virtual void GetFieldValue(ObjectComponent* component, void* value) const = 0;
-	virtual void AllocateValue(void*& value) const = 0;
-	virtual void ParseValue(const std::string& str, void* value) const = 0;
-	virtual void ValueToString(void* value, std::string& str) const = 0;
-
-private:
-	std::string m_ComponentName;
-	std::string m_FieldName;
-};
-
-template<typename FieldT>
-class ComponentFieldDefinitionTyped
-	: public ComponentFieldDefinition
-{
-public:
-	using FieldGetterT = std::function<void(const ObjectComponent*, FieldT&)>;
-	using FieldSetterT = std::function<void(ObjectComponent*, const FieldT&)>;
-
-	ComponentFieldDefinitionTyped(const std::string& component_name, const std::string& field_name, FieldGetterT getter, FieldSetterT setter);
-
-private:
-	virtual void SetFieldValue(ObjectComponent* component, void* value) const override;
-	virtual void GetFieldValue(ObjectComponent* component, void* value) const override;
-	virtual void AllocateValue(void*& value) const override;
-	virtual void ParseValue(const std::string& str, void* value) const override;
-	virtual void ValueToString(void* value, std::string& str) const override;
-
-private:
-	FieldGetterT m_Getter;
-	FieldSetterT m_Setter;
-};
+#include "ComponentFieldDefinition.h"
 
 class ComponentDefinition
 {
@@ -96,73 +50,4 @@ ComponentDefinition clazz::s_ComponentDefinition =		\
 	ComponentDefinition(#clazz, []()					\
 		{ return std::make_unique<clazz>(); }			\
 );
-
-#define DECLARE_FIELD(name)															\
-private:																			\
-	using name##_fieldType = decltype(name);										\
-	static ComponentFieldDefinitionTyped<name##_fieldType> name##_fieldDefinition;
-
-#define FIELD_TYPE(component, field) component##::##field##_fieldType
-
-#define DEFINE_FIELD(clazz, field_name)												\
-ComponentFieldDefinitionTyped<FIELD_TYPE(clazz, field_name)>						\
-clazz::field_name##_fieldDefinition =												\
-	ComponentFieldDefinitionTyped<FIELD_TYPE(clazz, field_name)>(					\
-		#clazz,																		\
-		#field_name,																\
-		[](const ObjectComponent* component, FIELD_TYPE(clazz, field_name)& value)	\
-		{																			\
-			const clazz* component_specific = static_cast<const clazz*>(component);	\
-			value = component_specific->field_name;									\
-		},																			\
-		[](ObjectComponent* component, const FIELD_TYPE(clazz, field_name)& value)	\
-		{																			\
-			clazz* component_specific = static_cast<clazz*>(component);				\
-			component_specific->field_name = value;									\
-		}																			\
-);
-
-
-
-template<typename FieldT>
-inline ComponentFieldDefinitionTyped<FieldT>::ComponentFieldDefinitionTyped(const std::string& component_name, const std::string& field_name, FieldGetterT getter, FieldSetterT setter)
-	: ComponentFieldDefinition(component_name, field_name)
-	, m_Getter(getter)
-	, m_Setter(setter)
-{
-}
-
-template<typename FieldT>
-inline void ComponentFieldDefinitionTyped<FieldT>::SetFieldValue(ObjectComponent* component, void* value) const
-{
-	FieldT* value_typed = reinterpret_cast<FieldT*>(value);
-	m_Setter(component, *value_typed);
-}
-
-template<typename FieldT>
-inline void ComponentFieldDefinitionTyped<FieldT>::GetFieldValue(ObjectComponent* component, void* value) const
-{
-	FieldT* value_typed = reinterpret_cast<FieldT*>(value);
-	m_Getter(component, *value_typed);
-}
-
-template<typename FieldT>
-inline void ComponentFieldDefinitionTyped<FieldT>::AllocateValue(void*& value) const
-{
-	value = new FieldT();
-}
-
-template<typename FieldT>
-inline void ComponentFieldDefinitionTyped<FieldT>::ParseValue(const std::string& str, void* value) const
-{
-	FieldT* value_typed = reinterpret_cast<FieldT*>(value);
-	ParseString<FieldT>(str, *value_typed);
-}
-
-template<typename FieldT>
-inline void ComponentFieldDefinitionTyped<FieldT>::ValueToString(void* value, std::string& str) const
-{
-	FieldT* value_typed = reinterpret_cast<FieldT*>(value);
-	str = ToString<FieldT>(*value_typed);
-}
 
