4bde6a88657d198196e7ed43ce61e895dd1c46fb
Jakub Stasiorowski <kuba.stasiorowski@wp.pl>
2024-04-13 19:48:08 +0200

    Fix assert functionality

--- FlatEngine/src/utility.h
+++ FlatEngine/src/utility.h
@@ -1,13 +1,9 @@
 #pragma once
 #include <stdio.h>
 
-#ifdef WIN32
-#define debug_break() // TODO
-#else
 #define debug_break() __debugbreak()
-#endif
 
-#ifdef _DEBUG
+#ifndef NDEBUG
 
 #define FE_ASSERT(cond, error_message, ...)		\
 		if (cond){}								\
