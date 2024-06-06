ea99f570ac3cf5078079ebf00b61fd9d275d68d7
Jakub Stasiorowski <kuba.stasiorowski@wp.pl>
2024-06-06 22:06:09 +0200

    Add unit tests for Transforming with scale and offset

--- src/FlintEngineTests/TransformTests.cpp
+++ src/FlintEngineTests/TransformTests.cpp
@@ -9,10 +9,11 @@
 
 constexpr float FLOAT_TOLERANCE = 0.001f;
 
-#define ASSERT_VEC_EQ(expected, actual)	\
-	ASSERT_TRUE((expected - actual).LengthSquared() < FLOAT_TOLERANCE*FLOAT_TOLERANCE);
+#define ASSERT_VEC_EQ(expected, actual)		\
+	ASSERT_FLOAT_EQ(expected.x, actual.x);	\
+	ASSERT_FLOAT_EQ(expected.y, actual.y);
 
-TEST(SUITE_NAME, IdentityTransformDoesNotChangePoint)
+TEST(SUITE_NAME, IdentityDoesNotChangePoint)
 {
 	// Arrange
 	const Vector pos(0, 0);
@@ -26,6 +27,59 @@ TEST(SUITE_NAME, IdentityTransformDoesNotChangePoint)
 
 	// Assert
 	ASSERT_VEC_EQ(in_point, out_point);
+}
+
+TEST(SUITE_NAME, OffsetedAddsOffset)
+{
+	// Arrange
+	const Vector pos(1, -2.5f);
+	const Vector scale(1, 1);
+	Transform transform(pos, scale);
+
+	const Vector in_point(5, -7.5f);
+
+	// Act
+	const Vector out_point = transform.TransformPoint(in_point);
+
+	// Assert
+	const Vector expected_point = pos + in_point;
+	ASSERT_VEC_EQ(expected_point, out_point);
+}
+
+TEST(SUITE_NAME, ScaledScalesOffset)
+{
+	// Arrange
+	const Vector pos(0, 0);
+	const Vector scale(0.5f, 2.7f);
+	Transform transform(pos, scale);
+
+	const Vector in_point(5, -7.5f);
+
+	// Act
+	const Vector out_point = transform.TransformPoint(in_point);
+
+	// Assert
+	const Vector expected_point = Vector(in_point.x * scale.x, in_point.y * scale.y);
+	ASSERT_VEC_EQ(expected_point, out_point);
+}
+
+TEST(SUITE_NAME, ScaleAndOffsetScalesAndOffsets)
+{
+	// Arrange
+	const Vector pos(1, -2.5f);
+	const Vector scale(0.5f, 2.7f);
+	Transform transform(pos, scale);
+
+	const Vector in_point(5, -7.5f);
+
+	// Act
+	const Vector out_point = transform.TransformPoint(in_point);
+
+	// Assert
+	const float expected_x = in_point.x * scale.x + pos.x;
+	const float expected_y = in_point.y * scale.y + pos.y;
+	ASSERT_EQ(expected_x, out_point.x);
+	ASSERT_EQ(expected_y, out_point.y);
 }
 
 #undef SUITE_NAME
