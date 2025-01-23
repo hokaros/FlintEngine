16e913a143bbfeb4f4e02c301bc62bd55c6dd7a0
Jakub Stasiorowski <kuba.stasiorowski@wp.pl>
2025-01-23 21:12:50 +0100

    Add MergeVertices tests

--- src/FlintEngineTests/VertexCollectionTests.cpp
+++ src/FlintEngineTests/VertexCollectionTests.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include <Navigation/VertexCollection.h>
+#include "TestHelpers.h"
 
 #ifdef SUITE_NAME
 #error Cannot redefine suite name
@@ -21,13 +22,12 @@ TEST(SUITE_NAME, RemovingFromStartAdjustsEdgeIndices)
 	edges.push_back(IndexPair(3, 1));
 
 	// Act
-	vCollection.RemoveVertex(vCollection.begin());
+	vCollection.RemoveVertex(0);
 
 	// Assert
 	ASSERT_EQ(IndexPair(0, 1), edges[0]);
 	ASSERT_EQ(IndexPair(2, 0), edges[1]);
 }
-
 
 TEST(SUITE_NAME, RemovingVertexRemovesEdgesAndAdjustsIndices)
 {
@@ -43,11 +43,66 @@ TEST(SUITE_NAME, RemovingVertexRemovesEdgesAndAdjustsIndices)
 	edges.push_back(IndexPair(3, 1));
 
 	// Act
-	vCollection.RemoveVertex(vCollection.begin());
+	vCollection.RemoveVertex(0);
 
 	// Assert
 	ASSERT_EQ(1, edges.size());
 	ASSERT_EQ(IndexPair(2, 0), edges[0]);
+}
+
+TEST(SUITE_NAME, MergingFreeVerticesKeepsOtherConnections)
+{
+	// Arrange
+	VertexCollection vCollection;
+	vCollection.AddVertex(Vector(0, 0));
+	vCollection.AddVertex(Vector(0, 1));
+	vCollection.AddVertex(Vector(1, 0));
+	vCollection.AddVertex(Vector(1, 1));
+
+	std::vector<IndexPair>& edges = vCollection.CreateAndGetEdgeCollection();
+	const size_t attach_idx1 = 2;
+	const Vector prev_attached_v1 = vCollection[attach_idx1];
+	const size_t attach_idx2 = 3;
+	const Vector prev_attached_v2 = vCollection[attach_idx2];
+	edges.push_back(IndexPair(attach_idx1, attach_idx2));
+	edges.push_back(IndexPair(attach_idx2, attach_idx1));
+
+	// Act
+	size_t merged_idx1 = 0;
+	size_t merged_idx2 = 1;
+	vCollection.MergeVertices(merged_idx1, merged_idx2, Vector(0, 0.5f));
+
+	// Assert
+	ASSERT_EQ(2, edges.size());
+	ASSERT_VEC_EQ(prev_attached_v1, vCollection[edges[0].first]);
+	ASSERT_VEC_EQ(prev_attached_v2, vCollection[edges[0].second]);
+	ASSERT_VEC_EQ(prev_attached_v2, vCollection[edges[1].first]);
+	ASSERT_VEC_EQ(prev_attached_v1, vCollection[edges[1].second]);
+}
+
+TEST(SUITE_NAME, MergingAttachedVerticesReconnects)
+{
+	// Arrange
+	VertexCollection vCollection;
+	vCollection.AddVertex(Vector(0, 0));
+	vCollection.AddVertex(Vector(0, 1));
+	vCollection.AddVertex(Vector(1, 1));
+	vCollection.AddVertex(Vector(1, 0));
+
+	std::vector<IndexPair>& edges = vCollection.CreateAndGetEdgeCollection();
+	edges.push_back(IndexPair(0, 1));
+	edges.push_back(IndexPair(3, 2));
+
+	// Act
+	size_t merged_idx1 = 0;
+	size_t merged_idx2 = 3;
+	const Vector merged_vertex = Vector(5, 5);
+	vCollection.MergeVertices(merged_idx1, merged_idx2, merged_vertex);
+
+	// Assert
+	ASSERT_EQ(2, edges.size());
+	ASSERT_VEC_EQ(merged_vertex, vCollection[edges[0].first]);
+	ASSERT_VEC_EQ(merged_vertex, vCollection[edges[1].first]);
 }
 
 #undef SUITE_NAME
